82325808d4256e1be4873926dcaf9005b3413910
PeterClaydon <peter.claydon@continuumbridge.com>
2014-05-20 17:07:34 +0100

    Initial version. Basic testing.

---
+++ wemo.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# wemoadaptor.py
+# Copyright (C) ContinuumBridge Limited, 2013-2014 - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Peter Claydon
+#
+ModuleName = "WeMo"
+
+import sys
+import time
+import os
+import logging
+from cbcommslib import CbAdaptor
+from cbconfig import *
+from twisted.internet import threads
+from twisted.internet import reactor
+from ouimeaux.environment import Environment
+
+class Adaptor(CbAdaptor):
+    def __init__(self, argv):
+        logging.basicConfig(filename=CB_LOGFILE,level=CB_LOGGING_LEVEL,format='%(asctime)s %(message)s')
+        self.state = "idle"
+        self.previousState = "off"
+        self.apps = []
+        #CbAdaprot.__init__ MUST be called
+        CbAdaptor.__init__(self, argv)
+
+    def setState(self, action):
+        if self.state == "idle":
+            if action == "connected":
+                self.state = "connected"
+            elif action == "inUse":
+                self.state = "inUse"
+        elif self.state == "connected":
+            if action == "inUse":
+                self.state = "activate"
+        elif self.state == "inUse":
+            if action == "connected":
+                self.state = "activate"
+        if self.state == "activate":
+            if not self.apps:
+                # No apps are using this switch
+                logging.info("%s %s No apps using switch", ModuleName, self.id)
+            elif self.sim == 0:
+                logging.debug("%s %s Activated", ModuleName, self.id)
+            self.state = "running"
+        logging.debug("%s %s state = %s", ModuleName, self.id, self.state)
+
+    def reportState(self, state):
+        logging.debug("%s %s Switch state = %s", ModuleName, self.id, state)
+        msg = {"id": self.id,
+               "timeStamp": time.time(),
+               "content": "switch_state",
+               "data": state}
+        for a in self.apps:
+            self.sendMessage(msg, a)
+
+    def onAppInit(self, message):
+        logging.debug("%s %s %s onAppInit, req = %s", ModuleName, self.id, self.friendly_name, message)
+        resp = {"name": self.name,
+                "id": self.id,
+                "status": "ok",
+                "functions": [{"parameter": "switch",
+                               "type": "240V",
+                               "purpose": "heater"}],
+                "content": "functions"}
+        self.sendMessage(resp, message["id"])
+
+    def onAppRequest(self, message):
+        logging.debug("%s %s %s onAppRequest, message = %s", ModuleName, self.id, self.friendly_name, message)
+        # Switch off anything that already exists for this app
+        if req["id"] not in self.apps:
+            if "switch" in req["functions"]:
+                self.apps.append(req["id"])  
+        else:
+            if "switch" not in req["functions"]:
+                self.apps.remove(req["id"])  
+        self.checkAllProcessed(message["id"])
+
+    def onOff(self, numState):
+        if numState == "1":
+            return "on"
+        else:
+            return "off"
+
+    def onAppCommand(self, message):
+        # If at first it doesn't succeed, try again.
+        for i in range(2):
+            if message["data"] == "on":
+                self.switch.on()
+                state = self.onOff(self.switch.get_state())
+                if state == "on":
+                    break
+            elif message["data"] == "off":
+                self.switch.off()
+                state = self.onOff(self.switch.get_state())
+                if state == "off":
+                    break
+        if state != self.previousState:
+            self.reportState(state)
+            self.previousState = state
+
+    def onConfigureMessage(self, config):
+        if not self.configured:
+            logging.info("%s %s %s Init", ModuleName, self.id, self.friendly_name)
+            try:
+                self.env = Environment()
+                logging.info("%s %s self.env = %s", ModuleName, self.id, str(self.env))
+                status = str(self.env.start())
+                logging.info("%s %s status = %s", ModuleName, self.id, status)
+                self.devices = self.env.list_switches()
+                logging.info("%s %s %s devices: %s", ModuleName, self.id, self.friendly_name, self.devices)
+                self.switch = self.env.get_switch(self.devices[0])
+                state = self.switch.get_state()
+                logging.info("%s %s %s Switch state on init: %s", ModuleName, self.id, self.friendly_name, state)
+                self.configured = True
+            except:
+                logging.warning("%s %s %s Unable to communicate with WeMo switch", ModuleName, self.id, self.friendly_name)
+                self.setState("error")
+
+if __name__ == '__main__':
+    adaptor = Adaptor(sys.argv)
+
